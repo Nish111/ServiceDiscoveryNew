709038a9c802ea547d44f263bb0fddd2e6c1f780
Nishant Banginwar <banginwar.nishant1@gmail.com>
2024-11-17 11:53:15 +0530

    Service Discovery

---
+++ src/test/java/org/example/ServiceDiscoveryNew/ServiceDiscoveryNewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ServiceDiscoveryNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
